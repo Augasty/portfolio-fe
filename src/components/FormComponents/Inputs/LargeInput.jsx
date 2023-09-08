345a4eb873b15f895964db57f5ec6e1487926c90
Augasty <sayaksengupta140@gmail.com>
2023-09-08 17:12:46 +0530

    css issue fixed

--- src/components/FormComponents/Inputs/LargeInput.jsx
+++ src/components/FormComponents/Inputs/LargeInput.jsx
@@ -4,7 +4,7 @@ const LargeInput = ({ title, formData, variable, setFormData }) => {
   return (
     <div className="w-full px-3">
       <label
-        className="block uppercase tracking-wide text-white-700 text-xs font-bold mb-2"
+        className="block uppercase tracking-wide text-white text-xs font-bold mb-2 "
         htmlFor="grid-password"
       >
         {title}
@@ -12,7 +12,7 @@ const LargeInput = ({ title, formData, variable, setFormData }) => {
       <div className="relative">
         <span className="absolute inset-y-0 left-0 pl-3 flex items-center text-gray-500" />
         <textarea
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded pt-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          className=" font-bold appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded pt-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           value={formData[variable]}
           onChange={(e) =>
             setFormData({ ...formData, [variable]: e.target.value })
