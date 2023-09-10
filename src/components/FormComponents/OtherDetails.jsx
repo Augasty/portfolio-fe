3e30a2c72ec04921ae23ee160279fd8ac62113dd
Augasty <sayaksengupta140@gmail.com>
2023-09-10 19:34:40 +0530

    minor change

--- src/components/FormComponents/OtherDetails.jsx
+++ src/components/FormComponents/OtherDetails.jsx
@@ -59,7 +59,7 @@ export default function OtherDetails() {
     <>
       <div className="text-center mb-16 my-10">
         <h3 className="Page heading text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-white">
-          experience <span className="text-indigo-600">Details</span>
+          Other <span className="text-indigo-600">Details</span>
         </h3>
       </div>
 
